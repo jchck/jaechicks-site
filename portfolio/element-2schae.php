3bd271470be185cfe9c44d2ef02d9e224fd57c6d
Justin Chick <hey@justinchick.com>
2014-06-20 15:03:02 -0500

    another typo [deploy: production]

--- portfolio/element-2schae.php
+++ portfolio/element-2schae.php
@@ -1,5 +1,5 @@
 <article class="item">
-	<a href="http://2schaecafe.com" target="_blank" class="image full"><img src="<?php echo JAECHICK_ROOT . '/assets/img/portfolio/2Schae.jpg' ?>"></a>
+	<a href="http://2schaecafe.com" target="_blank" class="image full"><img src="<?php echo JAECHICK_ROOT . '/assets/img/portfolio/2schae.jpg' ?>"></a>
 	<header>
 		<h3>2Schae Cafe</h3>
 		<a href="http://2schaecafe.com" target="_blank">View Site</a>
