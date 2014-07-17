3322a7868b6bd5ca2e5c2d89d3ad311107d550c6
Justin Chick <hey@justinchick.com>
2014-07-16 21:38:07 -0500

    no display footer on front page

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,9 @@
-<footer class="content-info" role="contentinfo">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
-</footer>
+<?php if (!is_front_page()){?>
+	<footer class="content-info" role="contentinfo">
+	  <div class="container">
+	    <?php dynamic_sidebar('sidebar-footer'); ?>
+	  </div>
+	</footer>
+<?php } ?>
 
 <?php wp_footer(); ?>
