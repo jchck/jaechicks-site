76e001c3f08599f745583856359ed82412b097b6
Justin Chick <hey@justinchick.com>
2014-07-16 21:10:13 -0500

    move home stuff to it's own template
    keep base.php clean as possible kthanx

--- base.php
+++ base.php
@@ -13,13 +13,7 @@
   ?>
 
   <?php if (is_front_page()){ ?>
-    <div class="bg">
-      <div class="table">
-        <h1>
-          <object type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/assets/img/logo.svg' ?>" class="logo"></object>
-        </h1>
-      </div>
-    </div>
+    <?php get_template_part( 'templates/home' ); ?>
   <?php } ?>
 
   <div class="wrap container" role="document">
---
+++ templates/home.php
@@ -0,0 +1,7 @@
+<div class="bg">
+  <div class="table">
+    <h1>
+      <object type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/assets/img/logo.svg' ?>" class="logo"></object>
+    </h1>
+  </div>
+</div>
