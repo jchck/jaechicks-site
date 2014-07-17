7062a44cf51fbb7280b0d2df989e2b942eef2170
Justin Chick <hey@justinchick.com>
2014-07-16 21:32:45 -0500

    display homepage nav menu

--- templates/home.php
+++ templates/home.php
@@ -1,7 +1,15 @@
 <div class="bg">
-  <div class="table">
-    <h1>
-      <object type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/assets/img/logo.svg' ?>" class="logo"></object>
-    </h1>
-  </div>
+	<div class="table">
+		<div>
+			<object type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/assets/img/logo.svg' ?>" class="logo"></object>
+
+			<ul>
+				<li>Biography</li>
+				<li>Portfolio</li>
+				<li>Notebook</li>
+				<li>Photography</li>
+				<li>Contact</li>
+			</ul>
+		</div>
+	</div>
 </div>
