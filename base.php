85a2d609da707a38ed578e05f32e0ae57f456df2
Justin Chick <hey@justinchick.com>
2014-07-16 20:37:16 -0500

    .bg opacity son

--- base.php
+++ base.php
@@ -16,9 +16,7 @@
     <div class="bg">
       <div class="table">
         <h1>
-          <object type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/assets/img/logo.svg' ?>" class="logo">
-            logo fall back support
-          </object>
+          <object type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/assets/img/logo.svg' ?>" class="logo"></object>
         </h1>
       </div>
     </div>
