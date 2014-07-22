39033c4a42be861bc13e11d51cf86ceba394e20f
Justin Chick <hey@justinchick.com>
2014-07-22 12:39:10 -0400

    split up .bg styles for portability & homepage specific use

--- templates/header-simple.php
+++ templates/header-simple.php
@@ -1,10 +1,12 @@
-<div class="bg">
-	<div class="table">
-	  <div>
-	    <a href="<?php echo esc_url(home_url('/')); ?>/">
-	      <object type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/assets/img/logo.svg' ?>" class="logo"></object>
-	    </a>
-	    <?php wp_nav_menu(array('theme_location' => 'primary_navigation')); ?>
-	  </div>
+<header>
+	<div class="bg">
+		<div class="table">
+		  <div>
+		    <a href="<?php echo esc_url(home_url('/')); ?>/">
+		      <object type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/assets/img/logo.svg' ?>" class="logo"></object>
+		    </a>
+		    <?php wp_nav_menu(array('theme_location' => 'primary_navigation')); ?>
+		  </div>
+		</div>
 	</div>
-</div>
+</header>
