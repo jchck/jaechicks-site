33994e1a11c82bbf47399b451810ba42fdb8aff9
Justin Chick <hey@justinchick.com>
2014-07-21 20:52:16 -0400

    home nav via primary navigation

--- templates/home.php
+++ templates/home.php
@@ -3,14 +3,8 @@
 		<div>
 			<object type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/assets/img/logo.svg' ?>" class="logo"></object>
 
-			<ul>
-				<li>Biography</li>
-				<li>methodology</li>
-				<li>Portfolio</li>
-				<li>Notebook</li>
-				<li>Photography</li>
-				<li>Contact</li>
-			</ul>
+			<?php wp_nav_menu(array('theme_location' => 'primary_navigation')); ?>
+
 		</div>
 	</div>
 </div>
