5a7b2bf96ec1df755da1fee531969e737db2ce25
Justin Chick <hey@justinchick.com>
2014-07-21 21:52:01 -0400

    page logo size

--- templates/header.php
+++ templates/header.php
@@ -1,9 +1,11 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container">
-    
+
     <div class="table">
       <div>
-        <object type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/assets/img/logo.svg' ?>" class="logo"></object>
+        <a href="<?php echo esc_url(home_url('/')); ?>/">
+          <object type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/assets/img/logo.svg' ?>" class="logo"></object>
+        </a>
       </div>
     </div>
 
