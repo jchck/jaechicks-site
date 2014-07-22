ae67e1f71e5c86eb4397d05d8c7ed8ef944232b7
Justin Chick <hey@justinchick.com>
2014-07-22 18:37:42 -0400

    turning things back on and getting spicific

--- assets/js/plugins/rand.head.bg.js
+++ assets/js/plugins/rand.head.bg.js
@@ -1,7 +1,7 @@
 // For random header background images
 // This approach was used in order to manipulate pseudo-elements
 var count = 15,
-	css = '.bg:after { background-image: url(/wp-content/themes/my-site/assets/img/bg/'+(Math.floor(Math.random()*(count))+1)+'.jpg); }',
+	css = '.home .bg:after { background-image: url(/wp-content/themes/my-site/assets/img/bg/'+(Math.floor(Math.random()*(count))+1)+'.jpg); }',
 	head = document.head || document.getElementsByTagName('head')[0],
 	style = document.createElement('style');
 
@@ -13,15 +13,3 @@ if (style.styleSheet){
 }
 head.appendChild(style);
 
-// var count = 15,
-// 	css = '.header { background-image: url(/wp-content/themes/my-site/assets/img/bg/'+(Math.floor(Math.random()*(count))+1)+'.jpg); }',
-// 	head = document.head || document.getElementsByTagName('head')[0],
-// 	style = document.createElement('style');
-
-// style.type = 'text/css';
-// if (style.styleSheet){
-// 	style.styleSheet.cssText = css;
-// } else {
-// 	style.appendChild(document.createTextNode(css));
-// }
-// head.appendChild(style);
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2155,7 +2155,7 @@ backgroundResize();;$(document).ready(function(){
 document.createElement("section");;// For random header background images
 // This approach was used in order to manipulate pseudo-elements
 var count = 15,
-	css = '.bg:after { background-image: url(/wp-content/themes/my-site/assets/img/bg/'+(Math.floor(Math.random()*(count))+1)+'.jpg); }',
+	css = '.home .bg:after { background-image: url(/wp-content/themes/my-site/assets/img/bg/'+(Math.floor(Math.random()*(count))+1)+'.jpg); }',
 	head = document.head || document.getElementsByTagName('head')[0],
 	style = document.createElement('style');
 
@@ -2167,18 +2167,7 @@ if (style.styleSheet){
 }
 head.appendChild(style);
 
-// var count = 15,
-// 	css = '.header { background-image: url(/wp-content/themes/my-site/assets/img/bg/'+(Math.floor(Math.random()*(count))+1)+'.jpg); }',
-// 	head = document.head || document.getElementsByTagName('head')[0],
-// 	style = document.createElement('style');
-
-// style.type = 'text/css';
-// if (style.styleSheet){
-// 	style.styleSheet.cssText = css;
-// } else {
-// 	style.appendChild(document.createTextNode(css));
-// }
-// head.appendChild(style);;/* ========================================================================
+;/* ========================================================================
  * DOM-based Routing
  * Based on http://goo.gl/EUTi53 by Paul Irish
  *
