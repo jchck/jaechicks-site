716dfa38cbe31dfcd7c320703bb3236ef3266f32
Justin Chick <hey@justinchick.com>
2014-07-22 12:15:10 -0400

    revert to original

--- templates/header.php
+++ templates/header.php
@@ -22,7 +22,7 @@
     <nav class="collapse navbar-collapse" role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav menu'));
+          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
         endif;
       ?>
     </nav>
