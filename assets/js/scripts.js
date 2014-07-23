38b745fd3ea7bce1ab00bb519b0370f4e134e8e7
Justin Chick <hey@justinchick.com>
2014-07-23 19:55:51 -0400

    remove margin's in nav. on internal pages

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2129,30 +2129,7 @@ function backgroundResize(){
 }
 $(window).resize(backgroundResize);
 $(window).focus(backgroundResize);
-backgroundResize();;// $(document).ready(function(){
-//    // cache the window object
-//    $window = $(window);
- 
-//    $('section[data-type="background"]').each(function(){
-//      // declare the variable to affect the defined data-type
-//      var $scroll = $(this);
-                     
-//       $(window).scroll(function() {
-//         // HTML5 proves useful for helping with creating JS functions!
-//         // also, negative value because we're scrolling upwards                            
-//         var yPos = -($window.scrollTop() / $scroll.data('speed'));
-         
-//         // background position
-//         var coords = '50% '+ yPos + 'px';
- 
-//         // move the background
-//         $scroll.css({ backgroundPosition: coords });   
-//       }); // end window scroll
-//    });  // end section function
-// }); // close out script
-
-// /* Create HTML5 element for IE */
-// document.createElement("section");;// For random header background images
+backgroundResize();;// For random header background images
 // This approach was used in order to manipulate pseudo-elements
 var count = 15,
 	css = '.bg:after { background-image: url(/wp-content/themes/my-site/assets/img/bg/'+(Math.floor(Math.random()*(count))+1)+'.jpg); }',
