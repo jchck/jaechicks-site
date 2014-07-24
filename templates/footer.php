edd86d1d5682758383ff6c8942f1d4fb144f3bd5
Justin Chick <hey@justinchick.com>
2014-07-23 20:22:10 -0400

    begin footer styles

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,13 @@
 <?php if (!is_front_page()){?>
-	<footer class="content-info" role="contentinfo">
-	  <div class="container">
-	  	
+<footer>
+	<div class="content-info" role="contentinfo">
+	  <div class="row">
+	  	<div class="foot-across">
+	  		<p>Made with love in STL</p>
+	  	</div>
 	  </div>
-	</footer>
+	</div>
+</footer>
 <?php } ?>
 
 <?php wp_footer(); ?>
