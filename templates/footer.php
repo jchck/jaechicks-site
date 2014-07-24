7a410b8b1b660777581c1c4add629e73ddf58b0a
Justin Chick <hey@justinchick.com>
2014-07-23 20:09:18 -0400

    de-register footer widget

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 <?php if (!is_front_page()){?>
 	<footer class="content-info" role="contentinfo">
 	  <div class="container">
-	    <?php dynamic_sidebar('sidebar-footer'); ?>
+	  	
 	  </div>
 	</footer>
 <?php } ?>
