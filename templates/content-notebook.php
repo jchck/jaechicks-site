779d249111d328538634e044c08a5a08679587c6
jaechick <hey@justinchick.com>
2014-09-02 23:05:21 -0500

    styles for notebook index

--- templates/content-notebook.php
+++ templates/content-notebook.php
@@ -5,11 +5,10 @@
 	while ($wp_query->have_posts()) : $wp_query->the_post();
 ?>
 
-<div class="row">
+<div class="notebook-entry">
 	<h3 class="col-sm-12"><a href="<?php echo the_permalink();?>"><?php the_title();?></a></h3>
 	<div class="col-sm-12"><?php echo the_excerpt();?></div>
 </div>
-<hr />
 <?php endwhile; ?>
 
 <?php wp_reset_query(); ?>
