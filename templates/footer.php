5440313f5e99f01ef9024908a7b58f4e4e36b056
jaechick <hey@justinchick.com>
2014-09-06 18:49:22 -0500

    social icon titles

--- templates/footer.php
+++ templates/footer.php
@@ -4,9 +4,9 @@
 	  <div class="row">
 	  	<div class="foot-across">
 	  		<ul class="list-inline">
-	  			<li><a href="https://www.linkedin.com/in/justinchick" target="_blank" title="LinkedIn"><i class="fa fa-linkedin"></i></a></li>
-	  			<li><a href="http://twitter.com/jaechick" target="_blank" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-	  			<li><a href="mailto:hey@justinchick.com" target="_blank" title="Email"><i class="fa fa-envelope"></i></a></li>
+	  			<li><a href="https://www.linkedin.com/in/justinchick" target="_blank" title="Justin Chick on LinkedIn"><i class="fa fa-linkedin"></i></a></li>
+	  			<li><a href="http://twitter.com/jaechick" target="_blank" title="Follow Justin Chick on Twitter"><i class="fa fa-twitter"></i></a></li>
+	  			<li><a href="mailto:hey@justinchick.com" target="_blank" title="Contact Justin Chick via Email"><i class="fa fa-envelope"></i></a></li>
 	  		</ul>
 	  		<p><a href="<?php echo home_url(); ?>">Made in STL</a> by Justin Chick &copy; <?php the_date( Y ); ?></p>
 	  	</div>
