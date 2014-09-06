448e3203b9b90450274ff6c52fff1570715524ea
jaechick <hey@justinchick.com>
2014-09-06 18:05:51 -0500

    single.php is looking damn good!

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,6 +1,6 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
-    <header>
+    <header class="page-header">
       <h1 class="entry-title"><?php the_title(); ?></h1>
     </header>
     <div class="entry-content">
