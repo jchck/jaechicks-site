5dcd7766c828a97d72eb7a02ccee72de25af7f01
jaechick <hey@justinchick.com>
2014-09-09 11:36:08 -0500

    contact page split into comums

--- template-contact.php
+++ template-contact.php
@@ -5,6 +5,13 @@ Template Name: Contact Template
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+	<?php get_template_part('templates/page', 'header'); ?>
+	<div class="row">
+		<div class="col-sm-6">
+			<?php get_template_part('templates/content', 'page'); ?>
+		</div>
+		<div class="col-sm-6">
+			<?php gravity_form('contact page', false, false); ?>
+		</div>
+	</div>
 <?php endwhile; ?>
