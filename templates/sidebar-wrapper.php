2da3f10accca3634352a96d6750f2175946d7170
jaechick <hey@justinchick.com>
2014-09-10 14:35:06 -0500

    add logo to sidebar

--- templates/sidebar-wrapper.php
+++ templates/sidebar-wrapper.php
@@ -1,5 +1,6 @@
 <div class="sidebar-wrapper">
-  <div class="sidebar-wrapper">
-    <?php wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'sidebar-nav')); ?>
-  </div>
+	<a href="<?php echo esc_url(home_url('/')); ?>/">
+		<object type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/assets/img/logo.svg' ?>" class="logo"></object>
+	</a>
+	<?php wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'sidebar-nav')); ?>
 </div>
