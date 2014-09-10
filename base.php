bf347192791b2e6c43b0196601621c70dd3edc0f
jaechick <hey@justinchick.com>
2014-09-10 12:57:14 -0500

    same nav. same

--- base.php
+++ base.php
@@ -10,32 +10,7 @@
 
 <div class="sidebar-wrapper">
   <div class="sidebar-wrapper">
-    <ul class="sidebar-nav">
-      <li class="sidebar-brand">
-        <a href="#">Start Bootstrap</a>
-      </li>
-      <li>
-        <a href="#">Dashboard</a>
-      </li>
-      <li>
-        <a href="#">Shortcuts</a>
-      </li>
-      <li>
-        <a href="#">Overview</a>
-      </li>
-      <li>
-        <a href="#">Events</a>
-      </li>
-      <li>
-        <a href="#">About</a>
-      </li>
-      <li>
-        <a href="#">Services</a>
-      </li>
-      <li>
-        <a href="#">Contact</a>
-      </li>
-    </ul>
+    <?php wp_nav_menu(array('theme_location' => 'primary_navigation')); ?>
   </div>
 </div>
 
