e5c447452b80beff836c904ac47a792e37e15b9c
jaechick <hey@justinchick.com>
2014-09-09 23:00:12 -0500

    create signup landing page

--- templates/header-simple.php
+++ templates/header-simple.php
@@ -5,7 +5,10 @@
 		    <a href="<?php echo esc_url(home_url('/')); ?>/">
 		      <object type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/assets/img/logo.svg' ?>" class="logo"></object>
 		    </a>
-		    <?php wp_nav_menu(array('theme_location' => 'primary_navigation')); ?>
+		    <?php if (! is_page('signup')) { ?>
+		    	<?php wp_nav_menu(array('theme_location' => 'primary_navigation')); ?>
+		    <?php } ?>
+		    
 		  </div>
 		</div>
 	</div>
