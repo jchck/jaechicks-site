18b8e44fd03c90c525f48c39349e92a6709cf323
jaechick <hey@justinchick.com>
2014-09-21 21:59:39 -0500

    put SVG in central location

--- templates/home.php
+++ templates/home.php
@@ -1,7 +1,7 @@
 <div class="bg">
 	<div class="table">
 		<div>
-			<a href="<?php echo home_url(); ?>"><object type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/assets/img/logo.svg' ?>" class="logo"></object></a>
+			<?php get_template_part( 'templates/logo' ); ?>
 
 			<?php wp_nav_menu(array('theme_location' => 'primary_navigation')); ?>
 
