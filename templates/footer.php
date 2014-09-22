562e83a586086ab795f5d9416566ed5dee841e5f
jaechick <hey@justinchick.com>
2014-09-21 21:52:06 -0500

    link to sign up page in footer callout

--- templates/footer.php
+++ templates/footer.php
@@ -10,7 +10,7 @@
 	  			<li><a href="mailto:hey@justinchick.com" target="_blank" title="Contact Justin Chick via Email"><i class="fa fa-envelope"></i></a></li>
 	  		</ul>
 	  		<?php if (!is_page('signup' )){ ?>
-	  			<p class="signup"><a href="#">Did you know I have a weekly newsletter?</a></p>
+	  			<p class="signup"><a href="<?php echo home_url( 'signup' ); ?>">Did you know I have a weekly newsletter?</a></p>
 	  		<?php } ?>
 	  		<p class="credit"><a href="<?php echo home_url(); ?>" title="Justin Chick is a freelance WordPress developer in St. Louis MO">Made in STL</a> by <a href="<?php echo home_url( 'biography' ); ?>" title="Be sur to update this later">Justin Chick</a> &copy; <?php the_date( Y ); ?></p>
 	  	</div>
