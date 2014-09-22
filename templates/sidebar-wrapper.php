0f43196bab8e244bdeff8742720ce1966dc16c09
jaechick <hey@justinchick.com>
2014-09-21 21:46:35 -0500

    media queries so side nag can work on all mobil's
    ...i think
    
    Signed-off-by: jaechick <hey@justinchick.com>

--- templates/sidebar-wrapper.php
+++ templates/sidebar-wrapper.php
@@ -1,6 +1,3 @@
 <div class="sidebar-wrapper">
-	<a href="<?php echo esc_url(home_url('/')); ?>/">
-		<object type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/assets/img/logo.svg' ?>" class="logo"></object>
-	</a>
 	<?php wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'sidebar-nav')); ?>
 </div>
