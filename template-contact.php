bc291f0e87063b98340ba019a65b8a1e002ca719
jaechick <hey@justinchick.com>
2014-10-08 15:37:32 -0500

    comment out contact btn's

--- template-contact.php
+++ template-contact.php
@@ -6,7 +6,7 @@ Template Name: Contact Template
 
 <?php while (have_posts()) : the_post(); ?>
 	<?php get_template_part('templates/page', 'header'); ?>
-	<div class="row">
+	<!-- <div class="row">
 		<div class="col-sm-12">
 			<ul class="list-inline">
 				<li><a class="btn btn-primary" href="http://twitter.com/jaechick" target="_blank">@jaechick</a></li>
@@ -14,7 +14,7 @@ Template Name: Contact Template
 				<li><a class="btn btn-primary" href="tel:13144883200" target="_blank">(314) 488-3200</a></li>
 			</ul>
 		</div>
-	</div>
+	</div> -->
 	<div class="row">
 		<div class="col-sm-6">
 			<?php get_template_part('templates/content', 'page'); ?>
