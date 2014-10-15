f50003e06eb64ab8b9609acd5b714297b527b38f
jaechick <hey@justinchick.com>
2014-10-15 16:47:39 -0500

    bunches of lil changes to the landing page

--- template-landing.php
+++ template-landing.php
@@ -8,3 +8,5 @@ Template Name: Landing Template
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
+
+<p class="back"><a href="<?php echo esc_url(home_url('/')); ?>/"><i class="fa fa-hand-o-left"></i> back to justinchick.com</a></p>
--- templates/footer.php
+++ templates/footer.php
@@ -3,13 +3,13 @@
 	<div class="content-info" role="contentinfo">
 	  <div class="row">
 	  	<div class="foot-across">
+	  		<?php if (!is_page_template( 'landing' )){ ?>
 	  		<ul class="list-inline">
 	  			<li><a href="https://www.linkedin.com/in/justinchick" target="_blank" title="Justin Chick on LinkedIn"><i class="fa fa-linkedin"></i></a></li>
 	  			<li><a href="http://twitter.com/jaechick" target="_blank" title="Follow Justin Chick on Twitter"><i class="fa fa-twitter"></i></a></li>
 	  			<li><a href="https://github.com/jaechick" target="_blank" title="See Justin Chick code on Github"><i class="fa fa-github"></i></a></li>
 	  			<li><a href="https://medium.com/@jaechick" target="_blank" title="Read with Justin Chick on Medium"><i class="fa fa-medium"></i></a></li>
 	  		</ul>
-	  		<?php if (!is_page('signup' )){ ?>
 	  			<p class="signup"><a href="<?php echo home_url( 'signup' ); ?>">Did you know I have a weekly newsletter?</a></p>
 	  		<?php } ?>
 	  		<p class="credit"><a href="<?php echo home_url(); ?>" title="Justin Chick is a freelance WordPress developer in St. Louis MO">Made in STL</a> by <a href="<?php echo home_url( 'biography' ); ?>" title="Be sur to update this later">Justin Chick</a> with <a href="http://www.justintakespictures.co/" target="_blank" title="Justin Takes Pictures - Things As I See Them"><i class="fa fa-heart-o"></i></a></p>
