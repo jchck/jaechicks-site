a17f1a0529754cbbb6148c11cba68c41c478f953
jaechick <hey@justinchick.com>
2014-10-18 19:07:18 -0500

    load option tree via bower

--- lib/ot.php
+++ lib/ot.php
@@ -3,4 +3,4 @@
 add_filter( 'ot_theme_mode', '__return_true' );
 add_filter( 'ot_show_pages', '__return_true' );
 
-require_once locate_template('option-tree/ot-loader.php');
+require_once locate_template('/assets/vendor/option-tree/ot-loader.php');
