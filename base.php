5c80d9d735e81618f160e6f32464183e16966049
jaechick <hey@justinchick.com>
2014-11-22 14:43:48 -0600

    a boat load of things that should have been committed earlier

--- base.php
+++ base.php
@@ -1,43 +1,37 @@
 <?php get_template_part('templates/head'); ?>
 <body <?php body_class(); ?>>
-<div class="site-wrap">
-<?php get_template_part( 'templates/sidebar', 'wrapper' ); ?>
-<div class="push-wrap">
-  <div class="it-all">
-  <a href="#" class="toggle-nav"><i class="fa fa-bars"></i></a>
+  <div class="site-wrap">
+    <?php get_template_part( 'templates/sidebar', 'wrapper' ); ?>
+      <div class="push-wrap">
+        <a href="#" class="toggle-nav"><i class="fa fa-bars"></i></a>
+        <?php get_template_part( 'templates/outdated', 'browser' ); ?>
 
-  <!--[if lt IE 8]>
-    <div class="alert alert-warning">
-      <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
-    </div>
-  <![endif]-->
+        <?php
+          if (!is_front_page()) {
+            do_action( 'get_header' );
+            get_template_part( 'templates/header', 'simple' );
+          } else {
+            get_template_part( 'templates/home' );
+        } ?>
 
-  <?php
-    if (!is_front_page()) {
-      do_action( 'get_header' );
-      get_template_part( 'templates/header', 'simple' );
-    } else {
-      get_template_part( 'templates/home' );
-    } ?>
+        <?php if (!is_front_page()){ ?>
+          <div class="wrap container" role="document">
+            <div class="content row">
+              <main class="main <?php echo roots_main_class(); ?>" role="main">
+                <?php include roots_template_path(); ?>
+              </main><!-- /.main -->
+              <?php if (roots_display_sidebar()) : ?>
+                <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
+                  <?php include roots_sidebar_path(); ?>
+                </aside><!-- /.sidebar -->
+              <?php endif; ?>
+            </div><!-- /.content -->
+          </div><!-- /.wrap -->
+        <?php } ?>
 
-  <?php if (!is_front_page()){ ?>
-    <div class="wrap container" role="document">
-      <div class="content row">
-        <main class="main <?php echo roots_main_class(); ?>" role="main">
-          <?php include roots_template_path(); ?>
-        </main><!-- /.main -->
-        <?php if (roots_display_sidebar()) : ?>
-          <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
-            <?php include roots_sidebar_path(); ?>
-          </aside><!-- /.sidebar -->
-        <?php endif; ?>
-      </div><!-- /.content -->
-    </div><!-- /.wrap -->
-  <?php } ?>
-
-  <?php get_template_part('templates/footer'); ?>
-</div><!-- /.it-all -->
-</div><!-- /.push-wrap -->
-</div><!-- /.site-wrap -->
+        <?php get_template_part('templates/footer'); ?>
+      </div><!-- /.push-wrap -->
+  </div><!-- /.site-wrap -->
 </body>
+
 </html>
--- templates/sidebar-wrapper.php
+++ templates/sidebar-wrapper.php
@@ -1,4 +1,4 @@
-<nav>
+<nav class="mobile">
 	<?php wp_nav_menu(array(
 		'theme_location' => 'primary_navigation', 
 		'menu_class' => 'sidebar-nav'
