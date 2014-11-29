07214fed084c704cc8f30514901bfda04cdbc12f
jaechick <hey@justinchick.com>
2014-11-29 10:15:29 -0600

    remove publish date from single.php

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,2 +1,1 @@
 <p class="byline author vcard"><?php echo __('Written by:', 'roots'); ?> <a href="<?php echo home_url( 'biography' ); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></p>
-<time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
