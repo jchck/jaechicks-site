3a04e2e52dd4a7ad3c9ad7896b70210a006d9644
jaechick <hey@justinchick.com>
2015-01-05 14:53:59 -0600

    use WP_Query to query the books

--- archive-books.php
+++ archive-books.php
@@ -1,5 +1,15 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
+<?php
+	$books = $wp_query;
+	$wp_query = null;
+	$wp_query = new $WP_Query();
+	$wp_query->query(
+		'posts_per_page=-1'
+	);
+	while ($wp_query->have_posts()) : $wp_query->the_post();
+?>
+
 <div class="table-responsive">
 	<table class="table table-hover">
 		<thead>
@@ -34,3 +44,4 @@
 	</table>
 </div>
 
+<?php endwhile; wp_reset_query(); ?>
