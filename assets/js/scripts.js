cd09dfa09ce69dc98f724d58a0b9e473ff49da8d
jaechick <hey@justinchick.com>
2015-01-18 21:56:28 -0600

    turn on fluid box for takes pictures

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1502,8 +1502,9 @@ head.appendChild(style);
 ;// Fluidbox
 
 $(function () {
-    $('.entry-content p a').fluidbox();
-    $('.entry-content .row a').fluidbox();
+    $('.entry-content p a').fluidbox(); // photos in entry content 
+    $('.entry-content .row a').fluidbox(); // photos in a row in entry content
+    $('figure a').fluidbox(); // photos in takes pictures
 });;/* ========================================================================
  * DOM-based Routing
  * Based on http://goo.gl/EUTi53 by Paul Irish
