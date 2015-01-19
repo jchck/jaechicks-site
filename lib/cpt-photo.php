8c17aa1571c176d18a754a4326d6615b9fde2412
jaechick <hey@justinchick.com>
2015-01-18 21:48:04 -0600

    refractor portfolio CPT into photo gallery

--- lib/cpt-photo.php
+++ lib/cpt-photo.php
@@ -10,21 +10,21 @@
 * @param array|string  See optional args description above.
 * @return object|WP_Error the registered post type object, or an error object
 */
-function portfolio() {
+function jaechick_photos() {
 
 	$labels = array(
-		'name'                => __( 'Portfolio', 'text-domain' ),
-		'singular_name'       => __( 'Portfolio Item', 'text-domain' ),
-		'add_new'             => _x( 'Add New Portfolio Item', 'text-domain', 'text-domain' ),
-		'add_new_item'        => __( 'Add New Portfolio Item', 'text-domain' ),
-		'edit_item'           => __( 'Edit Portfolio Item', 'text-domain' ),
-		'new_item'            => __( 'New Portfolio Item', 'text-domain' ),
-		'view_item'           => __( 'View Portfolio Item', 'text-domain' ),
-		'search_items'        => __( 'Search Portfolio Items', 'text-domain' ),
-		'not_found'           => __( 'No Portfolio Items found', 'text-domain' ),
-		'not_found_in_trash'  => __( 'No Portfolio Items found in Trash', 'text-domain' ),
-		'parent_item_colon'   => __( 'Parent Portfolio Item:', 'text-domain' ),
-		'menu_name'           => __( 'Portfolio Items', 'text-domain' ),
+		'name'                => __( 'Photo', 'text-domain' ),
+		'singular_name'       => __( 'Photo', 'text-domain' ),
+		'add_new'             => _x( 'Add New Photo', 'text-domain', 'text-domain' ),
+		'add_new_item'        => __( 'Add New Photo', 'text-domain' ),
+		'edit_item'           => __( 'Edit Photo', 'text-domain' ),
+		'new_item'            => __( 'New Photo', 'text-domain' ),
+		'view_item'           => __( 'View Photo', 'text-domain' ),
+		'search_items'        => __( 'Search Photos', 'text-domain' ),
+		'not_found'           => __( 'No Photos found', 'text-domain' ),
+		'not_found_in_trash'  => __( 'No Photos found in Trash', 'text-domain' ),
+		'parent_item_colon'   => __( 'Parent Photo:', 'text-domain' ),
+		'menu_name'           => __( 'Photos', 'text-domain' ),
 	);
 
 	$args = array(
@@ -52,7 +52,7 @@ function portfolio() {
 			)
 	);
 
-	register_post_type( 'portfolio', $args );
+	register_post_type( 'photos', $args );
 }
 
-add_action( 'init', 'portfolio' );
+add_action( 'init', 'jaechick_photos' );
