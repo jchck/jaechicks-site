f5858c528fd869ecbcd206c4c138f8cd7a20db03
jaechick <hey@justinchick.com>
2015-01-19 19:14:39 -0600

    remove ot references from functions

--- lib/ot.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-add_filter( 'ot_theme_mode', '__return_true' );
-add_filter( 'ot_show_pages', '__return_true' );
-
-require_once locate_template('/assets/vendor/option-tree/ot-loader.php');
