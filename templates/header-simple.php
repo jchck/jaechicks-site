aa61ac0ec58c8a83b65758f3953c4c774e80a812
jaechick <hey@justinchick.com>
2015-01-23 15:05:53 -0600

    final touches on 404

--- templates/header-simple.php
+++ templates/header-simple.php
@@ -2,7 +2,11 @@
 	<div class="bg">
 		<div class="table">
 		  <div>
-		    <?php get_template_part( 'templates/logo' ); ?>
+		  	<?php if (!is_404()) {
+		  		get_template_part( 'templates/logo' );
+		  	} else {
+		  		get_template_part( 'templates/logo', '404' );
+		  	} ?>
 		    <?php if (! is_page_template('template-landing.php' )) { ?>
 		    	<?php wp_nav_menu(array('theme_location' => 'primary_navigation')); ?>
 		    <?php } ?>
--- templates/logo.php
+++ templates/logo.php
@@ -1,7 +1,11 @@
 <a class="svg" href="<?php echo esc_url(home_url('/')); ?>/">
 	<?php if (current_theme_supports('github' )) { ?>
 		<i class="fa fa-spinner fa-spin fa-5x"></i>
-	<?php } else { ?> 
-		<object title="Justin Chick is a freelance WordPress developer in St. Louis MO" type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/assets/img/jaechick-logo.svg' ?>" class="logo"></object>
+	<?php } else { ?>
+		<?php if (!is_404()) { ?>
+			<object title="Justin Chick is a freelance WordPress developer in St. Louis MO" type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/assets/img/jaechick-logo.svg' ?>" class="logo"></object>
+		<?php } else { ?>
+			<object title="Justin Chick is a freelance WordPress developer in St. Louis MO" type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/assets/img/jaechick-logo-404.svg' ?>" class="logo"></object>
+			<?php } ?>
 	<?php } ?>
 </a>
