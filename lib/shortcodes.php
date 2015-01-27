1ca93d9150872bf4522c2f98e31c0d5f9b8a5e6e
jaechick <hey@justinchick.com>
2015-01-26 22:27:05 -0600

    make fitvids available via shortcode

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -8,3 +8,15 @@ add_shortcode('one_half_last', 'one_half_last');
 function one_half_last( $atts, $content = null ){
 	return '<div class="col-sm-6">' . do_shortcode($content) . '</div></div>';
 }
+add_shortcode('fitvids', 'fitvids');
+function fitvids( $atts, $embed = null ){
+	extract( shortcode_atts( array(
+		"embed" => 'null'
+		),
+		$atts ) );
+	$video_wrapper = '<div class="video-wrapper"><iframe width="853" height="480" src="';
+	$video_wrapper .= $embed;
+	$video_wrapper .= '" frameborder="0" allowfullscreen></iframe></div>';
+
+	return $video_wrapper;
+}
