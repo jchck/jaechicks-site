eff33d73b1049ff97b5ba11fd8eeddfac5fddba2
jaechick <hey@justinchick.com>
2015-02-05 11:14:33 -0600

    add shortcake use comment

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -8,6 +8,10 @@ add_shortcode('one_half_last', 'one_half_last');
 function one_half_last( $atts, $content = null ){
 	return '<div class="col-sm-6">' . do_shortcode($content) . '</div></div>';
 }
+
+/**
+ * shortcode format = [fitvids embed=//www.youtube.com/embed/1yqVD0swvWU?rel=0&amp;showinfo=0]
+ */
 add_shortcode('fitvids', 'fitvids');
 function fitvids( $atts, $embed = null ){
 	extract( shortcode_atts( array(
